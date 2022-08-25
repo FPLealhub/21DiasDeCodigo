62e92c780138304d0b962771ecf10d10a29657f4
F.P. Leal <fabricio.pereira.leal@gmail.com>
2022-08-25 19:07:52 -0300

    adding particles.html

---
+++ page/scripts/particles.js
@@ -0,0 +1,15 @@
+tsParticles.load('dia10', {
+  background: {
+    color: '#191970'
+  },
+  particles: {
+    links: {
+      enable: true,
+      opacity: 0.7
+    },
+
+    move: {
+      enable: true
+    }
+  }
+})
